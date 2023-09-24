6498cfef6ca9b450ce203a009af4c4fb408f7206
root <tak2anthony@gmail.com>
2023-09-23 17:30:06 -0700

    Task all

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,15 +1,16 @@
+/// <reference path="./crud.d.ts" />
 
 import { RowID, RowElement } from './interface';
-import CRUD from './crud';
+import * as CRUD from './crud';
 
 const row: RowElement = {
   firstName: 'Guillaume',
   lastName: 'Salva',
-}
+};
 
 const newRowID: RowID = CRUD.insertRow(row);
-row.age = 23;
-const updatedRow: RowElement = row;
+const updatedRow: RowElement = {...row, age: 23}
 
-CRUD.updateRow(newRowID, updatedRow);
+CRUD.updatedRow(newRowID, updatedRow);
+
 CRUD.deleteRow(newRowID);
